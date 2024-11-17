86c8fa72d42f6eeebe37f7ef3464f7c2638936db
Priyansh Balyan <priyanshbalyan@gmail.com>
2024-11-18 00:30:39 +0900

    fix date selector css

--- src/components/atoms/DateRangePicker.tsx
+++ src/components/atoms/DateRangePicker.tsx
@@ -5,28 +5,58 @@ import { DateRange as DateRangePickerComp, RangeKeyDict } from 'react-date-range
 import useGetICSData from '~/hooks/useGetICSData';
 
 type Props = {
-    handleSelect: (rangesByKey: RangeKeyDict) => void;
-    startDate: Date | null;
-    endDate: Date | null;
-    northVilla: boolean;
+  handleSelect: (rangesByKey: RangeKeyDict) => void;
+  startDate: Date | null;
+  endDate: Date | null;
+  northVilla: boolean;
 }
 
 const DateRangePicker = ({ handleSelect, startDate = null, endDate = null, northVilla }: Props) => {
-    const { data } = useGetICSData(northVilla)
+  const { data } = useGetICSData(northVilla)
 
-    const selectionRange = {
-        startDate: startDate ?? new Date(),
-        endDate: endDate ?? new Date(),
-        key: 'selection',
-    }
-    return (
-      <DateRangePickerComp
-        ranges={[selectionRange]}
-        onChange={handleSelect}
-        minDate={new Date()}
-        disabledDates={data}
-      />
-    ) 
+  const selectionRange = {
+    startDate: startDate ?? new Date(),
+    endDate: endDate ?? new Date(),
+    key: 'selection',
+  }
+
+  const classNames = `
+          [&>div]:bg-transparent 
+          ![background:transparent]
+          [&>div]:text-white
+          [&>*>*>*>button.rdrDayDisabled]:bg-transparent
+          [&>*>*>*>button.rdrDayDisabled>span>span]:text-lg
+          [&>*>*>*>button.rdrDayDisabled>span>span]:text-[#999999]
+          [&>*>*>*>button.rdrDayDisabled>span>span]:dark:text-gray-500
+          [&>*>*>*>button>span>span]:text-lg
+          [&>*>*>*>button>span>span]:font-semibold
+          [&>*>*>*>button>span>span]:dark:text-white
+          [&>*>*>*>button.rdrDayDisabled>span>span]:dark:text-white
+          [&>option]:bg-black
+          [&>div>div>span.rdrDateInput]:bg-transparent
+          [&>div>div>span.rdrDateInput]:dark:bg-black/40
+          [&>div>div>span.rdrDateInput]:backdrop-blur-md
+          [&>div>div>span.rdrDateInput>input]:text-sm
+          [&>div>div>span.rdrDateInput>input]:dark:text-white
+          [&>div>div>span.rdrDateInput>input]:text-black
+          [&>div.rdrMonthAndYearWrapper>button.rdrNextPrevButton]:bg-white/40
+          [&>div.rdrMonthAndYearWrapper>button.rdrNextPrevButton]:dark:bg-white/90
+          [&>.rdrMonthAndYearWrapper>button.rdrNextPrevButton]:backdrop-blur-md
+          [&>div.rdrMonthAndYearWrapper>span.rdrMonthAndYearPickers>span.rdrYearPicker>select]:text-sm
+          [&>div.rdrMonthAndYearWrapper>span.rdrMonthAndYearPickers>span.rdrYearPicker>select]:dark:text-white
+          [&>div.rdrMonthAndYearWrapper>span.rdrMonthAndYearPickers>span.rdrMonthPicker>select]:text-sm
+          [&>div.rdrMonthAndYearWrapper>span.rdrMonthAndYearPickers>span.rdrMonthPicker>select]:dark:text-white
+    `
+
+  return (
+    <DateRangePickerComp
+      ranges={[selectionRange]}
+      onChange={handleSelect}
+      minDate={new Date()}
+      disabledDates={data}
+      className={classNames}
+    />
+  )
 }
 
 export default DateRangePicker
