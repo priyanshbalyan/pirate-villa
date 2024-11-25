78201fcce8f49ba4ebdc00ef3678cb85ba958622
Priyansh Balyan <priyanshbalyan@gmail.com>
2024-11-25 22:27:50 +0900

    handle error

--- src/hooks/useGetDisabledDates.ts
+++ src/hooks/useGetDisabledDates.ts
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query"
 
 async function getDisabledDates(northVilla: boolean): Promise<Date[]> {
     const response = await fetch(`/api/calendar${northVilla ? '?north=true' : ''}`, { cache: 'no-store'});
+    if (!response.ok) throw new Error(await response.json())
     const json = await response.json()
     return json as Date[]
 }
