9840261865dc91079a765fcb7a9edb91d1a79951
Priyansh Balyan <priyanshbalyan@gmail.com>
2024-12-06 00:30:43 +0900

    fixes

--- app/api/texts/route.ts
+++ app/api/texts/route.ts
@@ -1,15 +1,10 @@
 import { NextResponse } from 'next/server';
 import { openDb } from '~/lib/db';
-import { verifyToken } from '~/lib/auth';
-import { Text, TextMap } from '~/types';
+import { Text } from '~/types';
 import { getTextMap } from '~/utils/utils';
 
 export async function GET(req: Request) {
-  const headers = req.headers;
-
   try {
-    verifyToken(headers);
-
     const db = await openDb();
     const texts = await db.all<Text[]>('SELECT * FROM texts');
 
