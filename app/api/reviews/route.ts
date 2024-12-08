2c780036607274aed8286e5cda6a8d587465d283
Priyansh Balyan <priyanshbalyan@gmail.com>
2024-12-08 18:39:26 +0900

    Add force dynamic

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import { openDb } from '~/lib/db';
 import { Review } from '~/types';
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(req: Request) {
   try {
     const db = await openDb();
